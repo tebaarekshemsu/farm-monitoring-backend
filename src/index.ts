101033b97caa060799244bb229929b8f80c3c88b
tebarek <tebarek29@gmail.com>
2024-12-11 03:05:13 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,8 @@ app.use(cors());
 app.use('/api', apiRoutes);
 
 const PORT = process.env.PORT || 5000;
-const server = app.listen();
+const server = app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
 
 io.attach(server);
