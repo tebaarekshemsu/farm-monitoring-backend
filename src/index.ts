31aceedd2f6c8611ac173f8e51270e011e58d716
tebarek <tebarek29@gmail.com>
2024-12-15 13:43:21 +0300

    some fix

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use('/api', apiRoutes);
+app.use('/*', apiRoutes);
 
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () => {
